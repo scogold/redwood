7f0efee8083578af4856a4f8045b2a68e52cc033
Scotty Goldin <scottzgoldin2@gmail.com>
2022-11-15 17:16:03 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,34 +4,56 @@ import React, { Component } from "react";
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {tripList: [{name:"new",distance:4,co2:8}], enteredName: "", enteredDist: 0};
+    this.updateTripList = this.updateTripList.bind(this);
   }
   
-
+  updateTripList(){
+    newTrip = {
+      name: this.state.enteredName,
+      distance: this.state.enteredDist,
+      co2: this.state.enteredDist*1.4
+    }
+    this.setState({ tripList: this.state.tripList.concat(newTrip)});
+  }
+  
+  updateEnteredName(event){
+    this.setState({enteredName:event.target.value })
+  }
+  updateEnteredDist(event){
+    this.setState({enteredDist:event.target.value })
+  }
+  
   render() {
     return (
       <div className="App">
         <h1>Project Redwood</h1>
         <h2>Track your Co2 Savings From Biking</h2>
         <hr />
-        <AddTrip />
+        <AddTrip enterFun={this.updateTripList} changeFun1={this.updateEnteredName} changeFun2={this.updateEnteredDist}/>
         <hr />
-        <TripHistory />
+        <TripHistory tripHistoryList={this.state.tripList}/>
       </div>
     );
   }
 }
 
 class AddTrip extends Component {
+
   constructor(props) {
     super(props);
   }
 
   render() {
+    const enterFun = this.props.enterFun;
+    const nameUpdateFun = this.props.changeFun1;
+    const distUpdateFun = this.props.changeFun2;
+    
     return(
       <div>
-        <input type="text" placeholder="Trip Name"/>
-        <input type="number" placeholder="Distance in km"/>
-        <button> Add Trip </button>
+        <input type="text" placeholder="Trip Name" onChange={nameUpdateFun}/>
+        <input type="number" placeholder="Distance in km" onChange={distUpdateFun}/>
+        <button onClick={enterFun}> Add Trip </button>
       </div>
     );
   }
@@ -42,8 +64,25 @@ class TripHistory extends Component {
     super(props);
   }
   render() {
+    const tripHistoryList = this.props.tripHistoryList;
+    
     return(
-      <div> See Old Trips Here </div>
+      <div>
+        <table border="1">
+          <tr>
+            <th> Name </th>
+            <th> Distance </th>
+            <th> Co2 Savings </th>
+          </tr>
+          {tripHistoryList.map((item) => (
+            <tr>
+              <th> {item.name} </th>
+              <th> {item.distance} </th>
+              <th> {item.co2} </th>
+            </tr>
+          ))}
+        </table>
+      </div>
     );
   }
 }
